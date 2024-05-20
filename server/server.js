1e7500df3a5c985a1fedd6f41bf88fe7d3251a66
Zaid Ahmad <zaidd250@gmail.com>
2024-05-20 17:57:35 -0600

    feat: v0 complete

--- server/server.js
+++ server/server.js
@@ -1,10 +1,39 @@
-const io = require('socket.io')(3000, {
-    cors: {
-        origin: 'https://5173-idx-google-docs-clone-1716226088741.cluster-t23zgfo255e32uuvburngnfnn4.cloudworkstations.dev/',
-        methods: ['GET', 'POST']
-    }
-})
+const mongoose = require("mongoose");
+const Document = require("./Document");
 
-io.on("connection", socket => {
-    console.log('Connected')
-})z
+mongoose
+    .connect("mongodb://localhost/google-docs-clone")
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("Failed to connect to MongoDB", err));
+
+const defaultValue = "";
+
+const io = require("socket.io")(3000, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    socket.on("get-document", async (documentId) => {
+        const document = await findOrCreateDocument(documentId);
+        socket.join(documentId);
+        socket.emit("load-document", document.data);
+
+        socket.on("send-changes", (delta) => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta);
+        });
+
+        socket.on("save-document", async (data) => {
+            await Document.findByIdAndUpdate(documentId, { data });
+        });
+    });
+});
+
+async function findOrCreateDocument(id) {
+    if (id == null) return;
+    const document = await Document.findById(id);
+    if (document) return document;
+    return await Document.create({ _id: id, data: defaultValue });
+}
