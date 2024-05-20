04b560af4e1cacae7b49f8f10205f5212d8d51f9
zaidd250 <zaidd250@gmail.com>
2024-05-20 18:18:38 +0000

    setup server, style client

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -1,8 +1,29 @@
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
-import { useCallback } from "react"
+import { useCallback, useEffect } from "react"
+import {io} from "socket.io-client"
 
-export default function TextEditor() {
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+]
+
+export default function TextEditor() { 
+
+  useEffect(() => {
+    const socket = io("https://3000-idx-google-docs-clone-1716226088741.cluster-t23zgfo255e32uuvburngnfnn4.cloudworkstations.dev/")
+    
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
 
   const wrapperRef = useCallback(wrapper => {
     if (wrapper == null) return
@@ -10,7 +31,7 @@ export default function TextEditor() {
     wrapper.innerHTML = ""
     const editor = document.createElement("div")
     wrapper.append(editor)
-    new Quill(editor, {theme: "snow"})
+    new Quill(editor, {theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS }}, )
   }, [])
   
     return (
