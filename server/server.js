bd5529b44bb6b3c538b980e949e96df0d6a6ef4b
Zaid Ahmad <zaidd250@gmail.com>
2024-05-22 21:29:05 -0600

    install cors module

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Document = require("./Document");
 const express = require("express");
+const cors = require("cors");
 const http = require("http");
 const { Server } = require("socket.io");
 
