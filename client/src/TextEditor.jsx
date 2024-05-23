14d3031a1dd9efb78ad1570a996c148ad90c15ef
Zaid Ahmad <zaidd250@gmail.com>
2024-05-22 20:01:26 -0600

    deploy: add deployed ws server url

--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -23,7 +23,7 @@ export default function TextEditor() {
     const [quill, setQuill] = useState();
 
     useEffect(() => {
-        const s = io("http://localhost:3000");
+        const s = io("https://google-docs-clone-production.up.railway.app");
         setSocket(s);
         return () => {
             s.disconnect();
